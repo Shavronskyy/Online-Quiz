f92c0eb7d8f4575e27e935dd03f8397cd40ac00b
Hekeem <79774156+hekeemje@users.noreply.github.com>
2022-10-06 17:51:52 +0300

    implemented JWT auth

---
+++ OnlineQuiz.Core/OnlineQuiz.Core.DAL/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineQuiz.Core.DAL.Entities
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
